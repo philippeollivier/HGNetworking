635428153bdae2b0386cde48d7d424e9bede3dbe
Baby Shark <evanmliu@gmail.com>
2022-08-09 09:32:33 -0700

    Uncommenting inputmanager

--- Assets/Scripts/HGNetworking/InputManager.cs
+++ Assets/Scripts/HGNetworking/InputManager.cs
@@ -1,108 +1,96 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-////Put your actions in this enum
-//public enum KeybindingActions
-//{
-//    Forward,
-//    Backward,
-//    Jump
-//}
-//public class InputManager : MonoBehaviour
-//{
-//    #region Singleton Design
-//    private static InputManager _instance;
+//Put your actions in this enum
+public enum KeybindingActions
+{
+    Forward,
+    Backward,
+    Jump
+}
+public class InputManager : MonoBehaviour
+{
+    #region Singleton Design
+    private static InputManager _instance;
 
-//    public static InputManager Instance { get { return _instance; } }
+    public static InputManager Instance { get { return _instance; } }
 
 
-//    private void Awake()
-//    {
-//        if (_instance != null && _instance != this)
-//        {
-//            Destroy(this.gameObject);
-//        }
-//        else
-//        {
-//            _instance = this;
-//        }
-//        DontDestroyOnLoad(this);
-//    }
-//    #endregion
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+    #endregion
 
-//    [SerializeField] public Keybinds keybinds;
+    [SerializeField] public Keybinds keybinds;
 
-//    public KeyCode GetKeyForAction(KeybindingActions keybindingAction)
-//    {
-//        foreach(Keybinds.KeybindingCheck keybindingCheck in keybinds.keybindingChecks)
-//        {
-//            if(keybindingCheck.keybindingAction == keybindingAction)
-//            {
-//                return keybindingCheck.keyCode;
-//            } 
-//        }
-//        return KeyCode.None;
-//    }
+    public KeyCode GetKeyForAction(KeybindingActions keybindingAction)
+    {
+        foreach (Keybinds.KeybindingCheck keybindingCheck in keybinds.keybindingChecks)
+        {
+            if (keybindingCheck.keybindingAction == keybindingAction)
+            {
+                return keybindingCheck.keyCode;
+            }
+        }
+        return KeyCode.None;
+    }
 
-//    public bool GetKeyDown(KeybindingActions key)
-//    {
-//        foreach (Keybinds.KeybindingCheck keybindingCheck in keybinds.keybindingChecks)
-//        {
-//            if (keybindingCheck.keybindingAction == key)
-//            {
-//                return Input.GetKeyDown(keybindingCheck.keyCode);
-//            }
-//        }
-//        return false;
-//    }
+    public bool GetKeyDown(KeybindingActions key)
+    {
+        foreach (Keybinds.KeybindingCheck keybindingCheck in keybinds.keybindingChecks)
+        {
+            if (keybindingCheck.keybindingAction == key)
+            {
+                return Input.GetKeyDown(keybindingCheck.keyCode);
+            }
+        }
+        return false;
+    }
 
-//    public BitArray GetKeys() 
-//    {
-//        BitArray flags = new BitArray(ECSComponent.InputComponent.keybindingChecks.Count);
-//        for(int i = 0; i < ECSComponent.InputComponent.keybindingChecks.Count; i++)
-//        {
-//            if(Input.GetKey(ECSComponent.InputComponent.keybindingChecks[i].keyCode))
-//            {
-//                flags[i] = true;
-//            }
-//        }
-//        return flags;
-//    }
+    public BitArray GetKeys()
+    {
+        BitArray flags = new BitArray(ECSComponent.InputComponent.keybindingChecks.Count);
+        for (int i = 0; i < ECSComponent.InputComponent.keybindingChecks.Count; i++)
+        {
+            if (Input.GetKey(ECSComponent.InputComponent.keybindingChecks[i].keyCode))
+            {
+                flags[i] = true;
+            }
+        }
+        return flags;
+    }
 
-//    public bool GetKeyUp(KeybindingActions key)
-//    {
-//        foreach (Keybinds.KeybindingCheck keybindingCheck in keybinds.keybindingChecks)
-//        {
-//            if (keybindingCheck.keybindingAction == key)
-//            {
-//                return Input.GetKeyUp(keybindingCheck.keyCode);
-//            }
-//        }
-//        return false;
-//    }
+    public bool GetKeyUp(KeybindingActions key)
+    {
+        foreach (Keybinds.KeybindingCheck keybindingCheck in keybinds.keybindingChecks)
+        {
+            if (keybindingCheck.keybindingAction == key)
+            {
+                return Input.GetKeyUp(keybindingCheck.keyCode);
+            }
+        }
+        return false;
+    }
 
-//    public bool GetKey(KeybindingActions key)
-//    {
-//        foreach (Keybinds.KeybindingCheck keybindingCheck in keybinds.keybindingChecks)
-//        {
-//            if (keybindingCheck.keybindingAction == key)
-//            {
-//                return Input.GetKey(keybindingCheck.keyCode);
-//            }
-//        }
-//        return false;
-//    }
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-        
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-        
-//    }
-//}
+    public bool GetKey(KeybindingActions key)
+    {
+        foreach (Keybinds.KeybindingCheck keybindingCheck in keybinds.keybindingChecks)
+        {
+            if (keybindingCheck.keybindingAction == key)
+            {
+                return Input.GetKey(keybindingCheck.keyCode);
+            }
+        }
+        return false;
+    }
+}
--- Assets/Scripts/HGNetworking/Keybinds.cs
+++ Assets/Scripts/HGNetworking/Keybinds.cs
@@ -1,16 +1,16 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//[CreateAssetMenu(fileName = "Keybinds", menuName = "Keybinds")]
-//public class Keybinds : ScriptableObject
-//{
-//    [System.Serializable]
-//    public class KeybindingCheck
-//    {
-//        public KeybindingActions keybindingAction;
-//        public KeyCode keyCode;
-//    }
+[CreateAssetMenu(fileName = "Keybinds", menuName = "Keybinds")]
+public class Keybinds : ScriptableObject
+{
+    [System.Serializable]
+    public class KeybindingCheck
+    {
+        public KeybindingActions keybindingAction;
+        public KeyCode keyCode;
+    }
 
-//    public KeybindingCheck[] keybindingChecks;
-//}
+    public KeybindingCheck[] keybindingChecks;
+}
